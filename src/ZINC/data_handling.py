7673167d300562e49ef3748b2bca323c652e7856
1044704 <121672967+1044704@users.noreply.github.com>
2023-04-06 16:52:08 +0100

    Delete src directory

--- src/ZINC/data_handling.py
+++
@@ -1,8 +0,0 @@
-import os
-from torch_geometric.datasets import ZINC
-DATA_PATH = '../../data'
-
-def get_zinc_data(split):
-  path = '../../data/ZINC'
-  dataset = ZINC(path,subset=True,split=split)
-  return dataset
